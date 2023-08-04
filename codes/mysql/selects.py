6de122d44a7281cb45c6272b6bda8963ad1ca159
estskyway <estskyway@gmail.com>
2023-08-04 18:01:32 +0900

    first commit

---
+++ codes/mysql/selects.py
@@ -0,0 +1,15 @@
+# mysql connect package
+# pip --> conda
+
+# connect Mysql
+# ip, port, username, password, database name
+
+# editor - statement
+
+# make select 
+
+# excute select query
+
+# return resultset and then display
+
+# close 
